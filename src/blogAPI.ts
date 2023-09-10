b4f2b3fc5e57ae0f8c3b9e649ae49b16e60ad740
fujisawa <fujisawa5523@gmail.com>
2023-09-10 21:35:15 +0900

    Fix typo in port number

--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -1,7 +1,9 @@
 import { Article } from './types';
 
 export const getAllArticles = async (): Promise<Article[]> => {
-  const res = await fetch('http://localhost:3001/posts', { cache: 'no-store' }); // SSR
+  const res = await fetch('http://localhost:30001/posts', {
+    cache: 'no-store',
+  }); // SSR
   const articles = await res.json();
   return articles;
 };
