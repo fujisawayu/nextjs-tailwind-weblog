f2349cf1d715415c831ad97150dda62d5b73fbcf
fujisawa <fujisawa5523@gmail.com>
2023-09-10 21:36:40 +0900

    Add process to display all articles

--- src/app/components/ArticleList.tsx
+++ src/app/components/ArticleList.tsx
@@ -12,9 +12,9 @@ const ArticleList = ({ articles }: ArticleListProps) => {
     <div>
       {articles.map((article) => (
         <article className="flex flex-col shadow my-4" key={article.id}>
-          <Link href="#" className="hover:opacity-75">
+          <Link href={`articles/${article.id}`} className="hover:opacity-75">
             <Image
-              src="https://source.unsplash.com/collection/1346951/1000x500?sig=1"
+              src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${article.id}`}
               alt=""
               width={12800}
               height={3000}
@@ -22,13 +22,13 @@ const ArticleList = ({ articles }: ArticleListProps) => {
           </Link>
           <div className="bg-white flex flex-col justify-start p-6">
             <Link
-              href="#"
+              href={`articles/${article.id}`}
               className="text-blue-700 text-sm font-bold uppercase pb-4"
             >
               Technology
             </Link>
             <Link
-              href="#"
+              href={`articles/${article.id}`}
               className="text-3xl font-bold hover:text-gray-700 pb-4"
             >
               Lorem Ipsum Dolor Sit Amet Dolor Sit Amet
@@ -36,14 +36,17 @@ const ArticleList = ({ articles }: ArticleListProps) => {
             <p className="text-sm pb-3 text-slate-900">
               By{' '}
               <Link
-                href="#"
+                href={`articles/${article.id}`}
                 className="text-slate-900 font-semibold hover:text-gray-800"
               >
                 hoge
               </Link>
               , Published on September 1st, 2023
             </p>
-            <Link href="#" className="text-slate-900 pb-6">
+            <Link
+              href={`articles/${article.id}`}
+              className="text-slate-900 pb-6"
+            >
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
               quis porta dui. Ut eu iaculis massa. Sed ornare ligula lacus, quis
               iaculis dui porta volutpat. In sit amet posuere magna..
