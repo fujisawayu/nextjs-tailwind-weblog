85c3023b970a59fffff38e59c368230e57545f90
fujisawa <fujisawa5523@gmail.com>
2023-09-13 22:55:57 +0900

    Add detail page display function in ISR from API

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,21 +1,23 @@
+import { getDetailArticle } from '@/blogAPI';
 import Image from 'next/image';
 import React from 'react';
 
-const Article = ({ params }: { params: { id: Number } }) => {
+const Article = async ({ params }: { params: { id: Number } }) => {
+  const detailArticle = await getDetailArticle(params.id);
+
   return (
     <div className="max-w-3xl mx-auto p-5">
       <Image
-        src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${params.id}`}
+        src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${detailArticle.id}`}
         alt=""
         width={12800}
         height={3000}
       />
-      <h1 className="text-4xl text-center mb-10 mt-10">Title area</h1>
+      <h1 className="text-4xl text-center mb-10 mt-10">
+        {detailArticle.title}
+      </h1>
       <div className="text-lg leading-relaxed text-justify">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt
-        reprehenderit ad, neque impedit blanditiis molestias aut voluptatem
-        tempore deleniti ea in officia praesentium dicta possimus molestiae
-        necessitatibus cum ipsum iusto.
+        {detailArticle.content}
       </div>
     </div>
   );
--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -1,4 +1,6 @@
+import { promises } from 'dns';
 import { Article } from './types';
+import { notFound } from 'next/navigation';
 
 export const getAllArticles = async (): Promise<Article[]> => {
   const res = await fetch('http://localhost:3001/posts', {
@@ -11,4 +13,23 @@ export const getAllArticles = async (): Promise<Article[]> => {
 
   const articles = await res.json();
   return articles;
+};
+
+// @see https://nextjs.org/docs/app/building-your-application/data-fetching/fetching-caching-and-revalidating
+export const getDetailArticle = async (id: Number): Promise<Article> => {
+  const res = await fetch(`http://localhost:3001/posts/${id}`, {
+    next: { revalidate: 60 },
+  }); // ISR
+
+  if (res.status === 404) {
+    notFound();
+  }
+  // if (!res.ok) {
+  //   throw new Error();
+  // }
+
+  // await new Promise((resolve) => setTimeout(resolve, 5000));
+
+  const article = await res.json();
+  return article;
 };
