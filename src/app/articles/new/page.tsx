20cd53ca137d8df26f308449ff3c611bfe06cc9f
fujisawa <fujisawa5523@gmail.com>
2023-09-20 23:14:53 +0900

    Add CreateArticle definition for create new page

--- src/app/articles/new/page.tsx
+++ src/app/articles/new/page.tsx
@@ -1,16 +1,33 @@
-import React from 'react';
+'use client';
 
-const page = () => {
+import { createArticle } from '@/blogAPI';
+import React, { useState } from 'react';
+
+const CreateArticle = () => {
+  const [url, setUrl] = useState<string>('');
+  const [title, setTitle] = useState<string>('');
+  const [content, setContent] = useState<string>('');
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    await createArticle(url, title, content);
+  };
+
   return (
     <div className="min-h-screen py-8 px-4 md:px-12">
       <h2 className="text-2xl font-bold mb-4">Create New</h2>
 
-      <form className="bg-slate-200 p-6 rounded shadow-lg">
+      <form
+        className="bg-slate-200 p-6 rounded shadow-lg"
+        onSubmit={handleSubmit}
+      >
         <div className="mb-4">
           <label className="text-gray-700 text-sm font-bold mb-2">URL</label>
           <input
             type="text"
             className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+            onChange={(e) => setUrl(e.target.value)}
           />
         </div>
         <div className="mb-4">
@@ -18,11 +35,15 @@ const page = () => {
           <input
             type="text"
             className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+            onChange={(e) => setTitle(e.target.value)}
           />
         </div>
         <div className="mb-4">
           <label className="text-gray-700 text-sm font-bold mb-2">Text</label>
-          <textarea className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none" />
+          <textarea
+            onChange={(e) => setContent(e.target.value)}
+            className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+          />
         </div>
 
         <button type="submit" className="py-2 px-4 rounded-md bg-orange-300">
@@ -33,4 +54,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default CreateArticle;
